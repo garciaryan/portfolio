7871e12ec54558dd2581bdf2906967281b3e97d1
Ryan Garcia <garciaryan007@yahoo.com>
2018-10-03 12:23:19 -0700

    adds icons to footer for mobile

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,11 +1,16 @@
 <template>
   <footer class="footer">
     <div class="content columns is-mobile">
+      <div class="column icons is-hidden-desktop is-hidden-tablet">
+        <a href="https://github.com/garciaryan"><i class="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/ryan-garcia-b98b66127/"><i class="fab fa-linkedin-in"></i></a>
+        <a href="https://www.facebook.com/ryan.garcia.3705"><i class="fab fa-facebook-square"></i></a>
+      </div>
       <div class="column logo-col">
         <img src="../assets/logo.svg" alt="logo" class="logo">
       </div>
       <div class="column">
-        <span class="name">rgarciadev<span class="copy">&copy;</span> front end development</span>
+        <span class="name">rgarciadev<span class="copy">&copy;</span> development</span>
         <div class="address">
           <span>Fresno, CA</span>
         </div>
@@ -32,8 +37,17 @@ export default {
       font-weight: 300;
     }
   }
+  .icons {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    a {
+      color: $purple;
+      font-size: 23px;
+    }
+  }
   .logo-col {
-    justify-content: flex-end;
+    justify-content: center;
     align-items: center;
     display: flex;
     .logo {
