6356c4d57953f7a05046415ef7c798bceb2500b9
Ryan Garcia <garciaryan007@yahoo.com>
2018-10-03 10:48:26 -0700

    validates form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,43 +3,44 @@
     <navBar />
     <div class="contact">
       <side-nav />
-      <h1 class="header">let's work together</h1>
-      <div class="container">
-        <div class="field">
-          <label class="label">Name</label>
-          <div class="control">
-            <input class="input" type="text" placeholder="Text input">
-          </div>
-        </div>
+      <div class="form-container">
+        <h1 class="header">let's work together</h1>
+        <div class="container">
+          <form action="https://formspree.io/ryan@rgarciadev.com" method="POST" @submit="checkForm">
 
-        <div class="field">
-          <label class="label">Email</label>
-          <div class="control has-icons-left has-icons-right">
-            <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
-            <span class="icon is-small is-right">
-              <i class="fas fa-exclamation-triangle"></i>
-            </span>
-          </div>
-          <p class="help is-danger">This email is invalid</p>
-        </div>
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" :class="{'is-danger': errors['Email required.']}" type="email" name="email" placeholder="hi@gmail.com" v-model="email">
+                <input type="hidden" name="_next" value="/home" />
+                <input type="hidden" name="_subject" value="New submission!" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
+                </span>
+                <span v-if="errors['Email required.']" class="icon is-small is-right">
+                  <i class="fas fa-exclamation-triangle"></i>
+                </span>
+              </div>
+              <p v-if="errors['Email required.']" class="help is-danger">This field is required.</p>
+            </div>
 
-        <div class="field">
-          <label class="label">Message</label>
-          <div class="control">
-            <textarea class="textarea" placeholder="Textarea"></textarea>
-          </div>
-        </div>
+            <div class="field">
+              <label class="label">Message</label>
+              <div class="control">
+                <textarea class="textarea" placeholder="What's up?" name="message" :class="{'is-danger': errors['Message required.']}" v-model="message"></textarea>
+                <span v-if="errors['Message required.']" class="icon is-small is-right">
+                  <i class="fas fa-exclamation-triangle"></i>
+                </span>
+                <p v-if="errors['Message required.']" class="help is-danger">This field is required.</p>
+              </div>
+            </div>
 
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-link">Submit</button>
-          </div>
-          <div class="control">
-            <button class="button is-text">Cancel</button>
-          </div>
+            <div class="field is-grouped">
+              <div class="control">
+                <button class="button is-link" type="submit">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -58,6 +59,28 @@ export default {
     navBar,
     sideNav,
     footerBar
+  },
+
+  data () {
+    return {
+      errors: [],
+      email: null,
+      message: null
+    }
+  },
+
+  methods: {
+    checkForm (e) {
+      if (this.email && this.message) {
+        return true
+      }
+      
+      this.errors = []
+      if (!this.email) this.errors.push('Email required.')
+      if (!this.message) this.errors.push('Message required.')
+      
+      e.preventDefault()
+    }
   }
 }
 </script>
@@ -69,19 +92,29 @@ export default {
   background-color: $lavender;
   animation: 1.5s slideUp;
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: center;
   .header {
     text-align: center;
     font-size: 35px;
     font-weight: 300;
   }
   .container {
-    width: 75%;
+    width: 100%;
+  }
+  .form-container {
+    width: 67%;
+    animation: fadeIn 2.2s;
+    display: flex;
+    align-items: center;
   }
 }
 @keyframes slideUp {
   from { margin-top: 100%; }
   to { margin-top: 0; }
+}
+@keyframes fadeIn {
+  0% {opacity: 0;}
+  66% {opacity: 0;}
+  100% {opacity: 1;}
 }
 </style>
